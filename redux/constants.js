3628a723832d444708042e665c8055670fc90dcc
Rafi <rafibasha.sk@gmail.com>
2024-10-02 10:02:59 +0530

    Feature1

--- redux/constants.js
+++ redux/constants.js
@@ -2,4 +2,5 @@ export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 export const USER_LIST = 'USER_LIST';
 export const GET_ALL_USERS = 'GET_ALL_USERS';
+export const GET_ALL_USERS_new = 'GET_ALL_USERS';
 
